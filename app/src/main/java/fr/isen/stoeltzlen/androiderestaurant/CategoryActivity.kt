897e42670e86a25ceef6bbbcaa3cd756d9067308
stoeltzlenal <alexis.stoel@gmail.com>
2021-01-25 15:37:34 +0100

    CORRECTION erreur dans le code

--- app/src/main/java/fr/isen/stoeltzlen/androiderestaurant/CategoryActivity.kt
+++ app/src/main/java/fr/isen/stoeltzlen/androiderestaurant/CategoryActivity.kt
@@ -1,28 +1,31 @@
 package fr.isen.stoeltzlen.androiderestaurant
-
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-
+enum class ItemType{
+    HOMEENTREE,
+    HOMEPLAT,
+    HOMEDESSERT
+}
 class CategoryActivity : AppCompatActivity () {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category)
-        Log.d( tag: "lifecycle", msg: "onCreate")
+        Log.d("lifecycle","onCreate")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d( tag: "lifecycle", msg: "onResume")
+        Log.d("lifecycle","onResume")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.d( tag: "lifecycle", msg: "onRestart")
+        Log.d("lifecycle","onRestart")
     }
-
     override fun onDestroy() {
         super.onDestroy()
-        Log.d( tag: "lifecycle", msg: "onDestroy")
+        Log.d("lifecycle","onDestroy")
     }
 }
--- app/src/main/java/fr/isen/stoeltzlen/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/stoeltzlen/androiderestaurant/HomeActivity.kt
@@ -1,8 +1,10 @@
 package fr.isen.stoeltzlen.androiderestaurant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import fr.isen.stoeltzlen.androiderestaurant.databinding.ActivityHomeBinding
 
 
 class HomeActivity : AppCompatActivity() {
@@ -10,33 +12,28 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
 
         binding.homeEntree.setOnClickListener{
-            val intent = Intent ( packageContext: this, CategoryActivity::class.java)
+            val intent = Intent ( this, CategoryActivity::class.java)
             intent.putExtra(CATEGORY_NAME, ItemType.HOMEENTREE)
             startActivity(intent)
         }
 
         binding.homePlat.setOnClickListener {
-            val intent = Intent ( packageContext: this, CategoryActivity::class.java)
+            val intent = Intent (  this, CategoryActivity::class.java)
             intent.putExtra(CATEGORY_NAME, ItemType.HOMEPLAT)
             startActivity(intent)
 
         }
 
         binding.homeDessert.setOnClickListener{
-            val intent = Intent ( packageContext: this, CategoryActivity::class.java)
+            val intent = Intent ( this, CategoryActivity::class.java)
             intent.putExtra(CATEGORY_NAME, ItemType.HOMEDESSERT)
             startActivity(intent)
         }
-
-
-
     }
-
-
-
-
+    companion object {
+        const val CATEGORY_NAME = "categoryname"
+    }
 }

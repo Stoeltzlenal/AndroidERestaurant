4eb4b858fff1f1dd460aa4e0c8d5371d1086997f
stoeltzlenal <alexis.stoel@gmail.com>
2021-02-01 11:34:05 +0100

    Ajout panier et ajout d'article

--- app/src/main/java/fr/isen/stoeltzlen/androiderestaurant/DetailActivity.kt
+++ app/src/main/java/fr/isen/stoeltzlen/androiderestaurant/DetailActivity.kt
@@ -2,14 +2,23 @@ package fr.isen.stoeltzlen.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import fr.isen.stoeltzlen.androiderestaurant.R
 import fr.isen.stoeltzlen.androiderestaurant.databinding.ActivityDetailBinding
+import fr.isen.stoeltzlen.androiderestaurant.models.Basket
+import fr.isen.stoeltzlen.androiderestaurant.models.BasketItem
 import fr.isen.stoeltzlen.androiderestaurant.models.Item
+
+import org.json.JSONObject
+import java.io.File
+import kotlin.math.max
 
 
 private lateinit var binding: ActivityDetailBinding
 class DetailActivity : AppCompatActivity() {
-
+    private var itemCount = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -22,5 +31,42 @@ class DetailActivity : AppCompatActivity() {
         dish.getAllPictures()?.let {
             binding.detailPager.adapter = DetailViewAdapter(this, it)
         }
+
+        binding.dishTitleTextView.text = dish.name
+
+        binding.less.setOnClickListener {
+            itemCount = max(1, itemCount - 1)
+            refreshShop(dish)
+        }
+
+        binding.more.setOnClickListener {
+            itemCount += 1
+            refreshShop(dish)
+        }
+
+        binding.shopButton.setOnClickListener {
+            addToBasket(dish, itemCount)
+        }
     }
+
+    private fun refreshShop(dish: Item) {
+        val price = itemCount * dish.prices.first().price.toFloat()
+        binding.itemCount.text = itemCount.toString()
+        binding.shopButton.text = "${getString(R.string.total)} $price€"
+    }
+
+    private fun addToBasket(dish: Item, count: Int) {
+        val basket = Basket.getBasket(this)
+        basket.addItem(BasketItem(dish, count))
+        basket.save(this)
+        val json = GsonBuilder().setPrettyPrinting().create().toJson(basket)
+        Log.d("basket", json)
+    }
+
+
 }
+
+
+
+
+
--- app/src/main/java/fr/isen/stoeltzlen/androiderestaurant/models/MenuResult.kt
+++ app/src/main/java/fr/isen/stoeltzlen/androiderestaurant/models/MenuResult.kt
@@ -1,6 +1,9 @@
 package fr.isen.stoeltzlen.androiderestaurant.models
 
+import android.content.Context
+import com.google.gson.GsonBuilder
 import com.google.gson.annotations.SerializedName
+import java.io.File
 import java.io.Serializable
 
 class MenuResult (val data: List<Category>){}
@@ -25,6 +28,8 @@ class Item(
     } else {
         null
     }
+
+    //fun getPrice()=prices[0].price.toDouble()
 }
 
 class Ingredient (@SerializedName("name_fr") val name:String):Serializable{}
@@ -39,5 +44,37 @@ class NetworkConstant {
     }
 }
 
+class Basket (val items: MutableList<BasketItem>): Serializable {
 
+    fun addItem(item: BasketItem) {
+        val existingItem = items.firstOrNull {
+            it.dish.name == item.dish.name
+        }
+        existingItem?.let {
+            existingItem.count += item.count
+        } ?: run {
+            items.add(item)
+        }
+    }
 
+    fun save(context: Context) {
+        val jsonFile = File(context.cacheDir.absolutePath + BASKET_FILE)
+        jsonFile.writeText(GsonBuilder().setPrettyPrinting().create().toJson(this))
+    }
+
+    companion object {
+        fun getBasket(context: Context): Basket {
+            val jsonFile = File(context.cacheDir.absolutePath + BASKET_FILE)
+            return if(jsonFile.exists()) {
+                val json = jsonFile.readText()
+                GsonBuilder().setPrettyPrinting().create().fromJson(json, Basket::class.java)
+            } else {
+                Basket(mutableListOf())
+            }
+        }
+
+        const val BASKET_FILE = "basket.json"
+    }
+}
+
+class BasketItem(val dish: Item, var count: Int): Serializable {}
